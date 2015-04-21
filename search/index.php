f0c1b323e51ce3241c1cc652d0e7393b68254571
Andy Stocchetti <ajstocchetti@gmail.com>
2015-04-20 23:16:36 -0400

    initial search page (layout only)

---
+++ search/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8">
+  <title>SustainApp | Search Barcodes</title>
+  <meta name="description" content="sustainability sustainable app for corporate social responsibility">
+  <meta name="author" content="JM2">
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- FONT
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="/css/normalize.css">
+  <link rel="stylesheet" href="/css/skeleton.css">
+  <link rel="stylesheet" href="/css/sustainapp.css">
+
+  <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="icon" type="image/png" href="/images/favicon.png">
+
+</head>
+<body>
+
+  <!-- Primary Page Layout
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <div class="topbanner row">
+      <div class="container">
+		<div class="one-third column">
+			<img src="images/logo.png">
+		</div>
+		<div class="two-thirds column" style="margin-top:25px;">
+			<h1>SustainApp</h1>
+			<h3>A Smarter Way to Shop</h3>
+			<p>SustainApp provides instant Social Responsibility ratings for consumers.
+			<br />All you have to do is scan the barcode.</p>  
+		</div>
+	  </div>
+    </div>
+
+  <div class="container">
+	<div class="row">
+		<div class="one-third column">
+			<!-- image search -->
+			<input type="file" accept="image/*" capture="camera">
+			<button type="submit">Scan Barcode</button>
+		</div>
+		<div class="one-third column">
+			<input type="number">
+			<button type="submit">Search Barcode</button>
+			<!-- barcode text box -->
+		</div>
+		<div class="one-third column">
+			<!-- company text box -->
+			<input type="search" >
+			<button type="submit">Search Company</button>
+			
+		</div>
+	</div>
+  </div>
+
+<!-- End Document
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+</body>
+</html>
