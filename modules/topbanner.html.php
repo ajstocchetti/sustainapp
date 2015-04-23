3786fe750a8fe2040251210d975b9c9e2aa1f685
Andy Stocchetti <ajstocchetti@gmail.com>
2015-04-22 23:35:22 -0400

    silly doc root issue. i really need to test better

--- modules/topbanner.html.php
+++ modules/topbanner.html.php
@@ -1,13 +1,13 @@
 <div class="topbanner row">
 	<div class="container">
 		<div class="one-third column">
-			<img src="images/logo.png" class="srslyfit">
+			<img src="/images/logo.png" class="srslyfit">
 		</div>
 		<div class="two-thirds column" style="margin-top:25px;">
 			<h1>SustainApp</h1>
 			<h3>A Smarter Way to Shop</h3>
 			<p>SustainApp provides instant Social Responsibility ratings for consumers.
-			<br />All you have to do is scan the barcode.</p>  
+			<br />All you have to do is scan a barcode.</p>  
 		</div>
 	</div>
 </div>
