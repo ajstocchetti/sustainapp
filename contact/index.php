150c591a9d9065c01f2ebaa79d5ebb0db303f167
tackypinkflamingos <ajstocchetti@gmail.com>
2015-07-28 22:25:32 +0000

    spacing on contact page

--- contact/index.php
+++ contact/index.php
@@ -19,17 +19,17 @@
 			<form action="//sustainapp.us10.list-manage.com/subscribe/post?u=df200316eed96d1cf0f966cd3&amp;id=8ce70e5d0e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 				<div id="mc_embed_signup_scroll">
 				<div class="row">
-					<div class="mc-field-group col-xs-12 col-sm-4">
+					<div class="mc-field-group col-xs-12 col-sm-4 col-lg-3">
 						<label for="mce-EMAIL">Email Address<span class="asterisk">*</span></label>
 						<br>
 						<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
 					</div>
-					<div class="mc-field-group col-xs-12 col-sm-4">
+					<div class="mc-field-group col-xs-12 col-sm-4 col-lg-3">
 						<label for="mce-FNAME">First Name</label>
 						<br>
 						<input type="text" value="" name="FNAME" id="mce-FNAME">
 					</div>
-					<div class="mc-field-group col-xs-12 col-sm-4">
+					<div class="mc-field-group col-xs-12 col-sm-4 col-lg-3">
 						<label for="mce-LNAME">Last Name</label>
 						<br>
 						<input type="text" value="" name="LNAME" id="mce-LNAME">
@@ -51,45 +51,43 @@
 			
 			
 			<br><br>
-			<div>
-				<h2>Support Us</h2>
-				<p>Do you like SustainApp? Does it add value to your life? Would you like to see it continue to develop? Use one of the buttons below to make a contribution to the future of SustainApp.</p>
-				<div class="col-xs-12 col-sm-4 col-lg-3">
-				<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-					<input type="hidden" name="cmd" value="_s-xclick">
-					<input type="hidden" name="hosted_button_id" value="5WZW2N9VQSZ3N">
-					<button type="submit" class="btnStd-full">Donate $2</button>
-				</form>
-				</div>
-				<div class="col-xs-12 col-sm-4 col-lg-3">
-				<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-					<input type="hidden" name="cmd" value="_s-xclick">
-					<input type="hidden" name="hosted_button_id" value="MRDPQ27Q2WW4U">
-					<button type="submit" class="btnStd-full">Donate $5</button>
-				</form>
-				</div>
-				<div class="col-xs-12 col-sm-4 col-lg-3">
-				<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-					<input type="hidden" name="cmd" value="_s-xclick">
-					<input type="hidden" name="hosted_button_id" value="BB7P2ULGH7H5J">
-					<button type="submit" class="btnStd-full">Other Amount</button>
-				</form>
-				</div>
+			<h2>Support Us</h2>
+			<p>Do you like SustainApp? Does it add value to your life? Would you like to see it continue to develop? Use one of the buttons below to make a contribution to the future of SustainApp.</p>
+		<div class="row">
+			<div class="col-xs-12 col-sm-4 col-lg-3">
+			<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+				<input type="hidden" name="cmd" value="_s-xclick">
+				<input type="hidden" name="hosted_button_id" value="5WZW2N9VQSZ3N">
+				<button type="submit" class="btnStd-full">Donate $2</button>
+			</form>
 			</div>
-			
+			<div class="col-xs-12 col-sm-4 col-lg-3">
+			<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+				<input type="hidden" name="cmd" value="_s-xclick">
+				<input type="hidden" name="hosted_button_id" value="MRDPQ27Q2WW4U">
+				<button type="submit" class="btnStd-full">Donate $5</button>
+			</form>
+			</div>
+			<div class="col-xs-12 col-sm-4 col-lg-3">
+			<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+				<input type="hidden" name="cmd" value="_s-xclick">
+				<input type="hidden" name="hosted_button_id" value="BB7P2ULGH7H5J">
+				<button type="submit" class="btnStd-full">Other Amount</button>
+			</form>
+		</div>
+			</div>
+
 			
 			<br><br>
-			<div>
 			<h2>Follow Us</h2>
 				<a href="https://www.facebook.com/SustainApp" target="_blank"><img src="/images/facebook_header.png" alt="Like us on Facebook"></a>
 				&nbsp; &nbsp; &nbsp;
 				<a href="https://www.twitter.com/mysustainapp" target="_blank"><img src="/images/twitter_header.png" alt="Follow us on Twitter"></a>
 				&nbsp; &nbsp; &nbsp;
-				<a href=//www.pinterest.com/pin/create/button/?url=http%3A%2F%2Fwww.sustainapp.com&media=http%3A%2F%2Fwww.sustainapp.com%2Fimages%2Flogo.png&description=SustainApp!%20Instant%20CSR%20rankings%20for%20over%20thousands%20of%20companies" target="_blank"><img src="/images/pinterest_header.png" alt="Pin Us"></a>
+				<a href="//www.pinterest.com/pin/create/button/?url=http%3A%2F%2Fwww.sustainapp.com&media=http%3A%2F%2Fwww.sustainapp.com%2Fimages%2Flogo.png&description=SustainApp!%20Instant%20CSR%20rankings%20for%20over%20thousands%20of%20companies" target="_blank"><img src="/images/pinterest_header.png" alt="Pin Us"></a>
 			<br />
 			<br />
 			<p>What type of new hip company would we be without a social media campaign? And how can we hope to make a difference without you: friends, family, supporters, and those who want to make a positive impact. So do your thing, and connect with us so we can show the world how we're all working together to make a difference.</p>
-			</div>
 		</div>
 	</div> <!-- /container -->
 
