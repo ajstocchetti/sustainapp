ddd361d1a30aecf0c0112abc1b9d07b8d2d9d3fe
tackypinkflamingos <ajstocchetti@gmail.com>
2015-08-04 22:04:55 +0000

    made contact 2 pages

--- contact/index.php
+++ contact/index.php
@@ -6,83 +6,73 @@
 	include($_SERVER['DOCUMENT_ROOT']."/modules/header_nav.html.php");
 ?>
 	<div class="container">
-		<div class="col-xs-12">
-			<h2>Contact us</h2>
-			<p>If you have any questions or want to get involved, you can send us an email at <a href="mailto:info@sustainapp.com">info@sustainapp.com</a>. Also, join our mailing list through the form below. We'll keep you up to date on our progress, releases, and any important news. And don't worry, we promise not to sell your data or do anything like that. (What would that do for <i>our</i> CSR&#8253;)</p>
-
-			<!-- Begin MailChimp Signup Form -->
-			<div id="mc_embed_signup">
-			<!--
-				<link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
-				<style type="text/css">#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }</style>
-			-->
-			<form action="//sustainapp.us10.list-manage.com/subscribe/post?u=df200316eed96d1cf0f966cd3&amp;id=8ce70e5d0e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-				<div id="mc_embed_signup_scroll">
-				<div class="row">
-					<div class="mc-field-group col-xs-12 col-sm-4 col-lg-3">
-						<input type="email" value="" name="EMAIL" class="btnStd required email" id="mce-EMAIL" placeholder="Email [required]">
-					</div>
-					<div class="mc-field-group col-xs-12 col-sm-4 col-lg-3">
-						<input type="text" value="" name="FNAME" class="btnStd" id="mce-FNAME" placeholder="First Name">
-					</div>
-					<div class="mc-field-group col-xs-12 col-sm-4 col-lg-3">
-						<input type="text" value="" name="LNAME" class="btnStd" id="mce-LNAME" placeholder="Last Name">
-					</div>
-				</div>
-				<div id="mce-responses" class="col-xs-12">
-					<div class="response" id="mce-error-response" style="display:none"></div>
-					<div class="response" id="mce-success-response" style="display:none"></div>
-				</div>
-				<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-				<div style="position: absolute; left: -5000px;"><input type="text" name="b_df200316eed96d1cf0f966cd3_8ce70e5d0e" tabindex="-1" value=""></div>
-				<br>
-				<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btnStd-full">
-				</div>
-			</form>
-			<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
-			<script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
-			</div><!--end mailchimp mc_embed_signup-->
-			
-			
-			<br><br>
-			<h2>Support Us</h2>
-			<p>Do you like SustainApp? Does it add value to your life? Would you like to see it continue to develop? Use one of the buttons below to make a contribution to the future of SustainApp.</p>
 		<div class="row">
-			<div class="col-xs-12 col-sm-4 col-lg-3">
-			<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-				<input type="hidden" name="cmd" value="_s-xclick">
-				<input type="hidden" name="hosted_button_id" value="5WZW2N9VQSZ3N">
-				<button type="submit" class="btnStd-full">Donate $2</button>
-			</form>
+			<div class="col-xs-12 col-sm-6">
+				<h2>Contact us</h2>
+				<p>If you have any questions or want to get involved, you can send us an email at <a href="mailto:info@sustainapp.com">info@sustainapp.com</a>. Also, join our mailing list through the form below. We'll keep you up to date on our progress, releases, and any important news. And don't worry, we promise not to sell your data or do anything like that. (What would that do for <i>our</i> CSR&#8253;)</p>
+				<!-- Begin MailChimp Signup Form -->
+				<div id="mc_embed_signup">
+					<!--
+					<link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
+					<style type="text/css">#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }</style>
+					-->
+					<form action="//sustainapp.us10.list-manage.com/subscribe/post?u=df200316eed96d1cf0f966cd3&amp;id=8ce70e5d0e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+						<div id="mc_embed_signup_scroll">
+							<div class="mc-field-group">
+								<input type="email" value="" name="EMAIL" class="btnStd required email" id="mce-EMAIL" placeholder="Email [required]">
+							</div>
+							<input type="text" value="" name="FNAME" class="btnStd" id="mce-FNAME" placeholder="First Name">
+							<br>
+							<input type="text" value="" name="LNAME" class="btnStd" id="mce-LNAME" placeholder="Last Name">
+							<br>
+							<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btnStd-full">
+						</div>
+						<div id="mce-responses">
+							<div class="response" id="mce-error-response" style="display:none"></div>
+							<div class="response" id="mce-success-response" style="display:none"></div>
+						</div>
+						<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+						<div style="position: absolute; left: -5000px;"><input type="text" name="b_df200316eed96d1cf0f966cd3_8ce70e5d0e" tabindex="-1" value=""></div>
+					</form>
+					<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
+					<script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+				</div><!--end mailchimp mc_embed_signup-->
 			</div>
-			<div class="col-xs-12 col-sm-4 col-lg-3">
-			<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-				<input type="hidden" name="cmd" value="_s-xclick">
-				<input type="hidden" name="hosted_button_id" value="MRDPQ27Q2WW4U">
-				<button type="submit" class="btnStd-full">Donate $5</button>
-			</form>
-			</div>
-			<div class="col-xs-12 col-sm-4 col-lg-3">
-			<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-				<input type="hidden" name="cmd" value="_s-xclick">
-				<input type="hidden" name="hosted_button_id" value="BB7P2ULGH7H5J">
-				<button type="submit" class="btnStd-full">Other Amount</button>
-			</form>
-		</div>
-			</div>
-
 			
-			<br><br>
-			<h2>Follow Us</h2>
-				<a href="https://www.facebook.com/SustainApp" target="_blank"><img src="/images/facebook_header.png" alt="Like us on Facebook"></a>
-				&nbsp; &nbsp; &nbsp;
-				<a href="https://www.twitter.com/mysustainapp" target="_blank"><img src="/images/twitter_header.png" alt="Follow us on Twitter"></a>
-				&nbsp; &nbsp; &nbsp;
-				<a href="//www.pinterest.com/pin/create/button/?url=http%3A%2F%2Fwww.sustainapp.com&media=http%3A%2F%2Fwww.sustainapp.com%2Fimages%2Flogo.png&description=SustainApp!%20Instant%20CSR%20rankings%20for%20over%20thousands%20of%20companies" target="_blank"><img src="/images/pinterest_header.png" alt="Pin Us"></a>
-			<br />
-			<br />
-			<p>What type of new hip company would we be without a social media campaign? And how can we hope to make a difference without you: friends, family, supporters, and those who want to make a positive impact. So do your thing, and connect with us so we can show the world how we're all working together to make a difference.</p>
-		</div>
+			<div class="col-xs-12 col-sm-6">
+				<h2>Support Us</h2>
+				<p>Do you like SustainApp? Does it add value to your life? Would you like to see it continue to develop? Use one of the buttons below to make a contribution to the future of SustainApp.</p>	
+				<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+					<input type="hidden" name="cmd" value="_s-xclick">
+					<input type="hidden" name="hosted_button_id" value="5WZW2N9VQSZ3N">
+					<button type="submit" class="btnStd-full">Donate $2</button>
+				</form>
+				<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+					<input type="hidden" name="cmd" value="_s-xclick">
+					<input type="hidden" name="hosted_button_id" value="MRDPQ27Q2WW4U">
+					<button type="submit" class="btnStd-full">Donate $5</button>
+				</form>
+				<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+					<input type="hidden" name="cmd" value="_s-xclick">
+					<input type="hidden" name="hosted_button_id" value="BB7P2ULGH7H5J">
+					<button type="submit" class="btnStd-full">Other Amount</button>
+				</form>
+			</div>
+		</div><!-- end top row -->
+
+		<div class="row">
+			<div class="col-xs-12">
+				<h2>Follow Us</h2>
+					<a href="https://www.facebook.com/SustainApp" target="_blank"><img src="/images/facebook_header.png" alt="Like us on Facebook"></a>
+					&nbsp; &nbsp; &nbsp;
+					<a href="https://www.twitter.com/mysustainapp" target="_blank"><img src="/images/twitter_header.png" alt="Follow us on Twitter"></a>
+					&nbsp; &nbsp; &nbsp;
+					<a href="//www.pinterest.com/pin/create/button/?url=http%3A%2F%2Fwww.sustainapp.com&media=http%3A%2F%2Fwww.sustainapp.com%2Fimages%2Flogo.png&description=SustainApp!%20Instant%20CSR%20rankings%20for%20over%20thousands%20of%20companies" target="_blank"><img src="/images/pinterest_header.png" alt="Pin Us"></a>
+				<br />
+				<br />
+				<p>What type of new hip company would we be without a social media campaign? And how can we hope to make a difference without you: friends, family, supporters, and those who want to make a positive impact. So do your thing, and connect with us so we can show the world how we're all working together to make a difference.</p>
+			</div>
+		</div><!-- row -->
 	</div> <!-- /container -->
 
 <?php
